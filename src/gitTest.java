d206a41a2fdd89bdde880dd5987d4665b4d5a628
krist <vujnovic.kristina1@gmail.com>
2022-02-24 13:08:46 +0100

    Treći commit

--- src/gitTest.java
+++ src/gitTest.java
@@ -2,5 +2,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello Kristina!");
         System.out.println("Test2");
+        System.out.println("Ovo je već treći komit");
     }
 }
